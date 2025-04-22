b3e9241a4b90cd0834f7817f4ebcd2665ce5c4b1
Christopher Semler <csemler82@gmail.com>
2025-04-21 21:31:27 -0700

    created aircraft class, and more edits to main window mostly fixing errors

---
+++ Models/Aircraft.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AviationTaskManager.Models
+    {
+    public class Aircraft
+        {
+
+        public int AircraftId { get; set; } // Primary key
+        public string TailNumber { get; set; } // Unique identifier
+        public string Model { get; set; } // Aircraft type
+        public double TotalTime { get; set; } // ACTT (Aircraft Total Time)
+        }
+
+    }
+    
--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,14 +1,7 @@
-﻿using AviationTaskManager.ViewModels;
-using System.Text;
+﻿using AviationTaskManager.Models;
+using AviationTaskManager.ViewModels;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AviationTaskManager
 {
@@ -21,6 +14,21 @@ namespace AviationTaskManager
         {
             InitializeComponent();
             DataContext = new MainWindowViewModel(); // Correct ViewModel
+        }
+
+
+        private void AircraftSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+            {
+            if (AircraftSelector.SelectedItem is Aircraft selectedAircraft)
+                {
+                AircraftModel.Content = selectedAircraft.Model;
+                AircraftACTT.Content = selectedAircraft.TotalTime.ToString();
+                }
+            else
+                {
+                AircraftModel.Content = "N/A";
+                AircraftACTT.Content = "N/A";
+                }
             }
-    }
+        }
 }
